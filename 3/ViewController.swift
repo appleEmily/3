dc6de472fff0b224a675f1e096cdb086088f8bc9
appleEmily <emiri3128@gmail.com>
2019-06-05 17:27:30 +0900

    complete

--- 3/ViewController.swift
+++ 3/ViewController.swift
@@ -13,16 +13,22 @@ class minion {
     var minionName: String!
 }
 class ViewController: UIViewController {
-    var number: Int = 2
-    
-    var minionCollection: [String] = []
-    var kevin: minion = minion()
-    var stuart: minion = minion()
-    var bob: minion = minion()
+//    var number: Int = 2
+//    var minionCollection: [minion] = []
+//    //型はminion
+//    var kevin: minion = minion()
+//    var stuart: minion = minion()
+//    var bob: minion = minion()
     
     @IBOutlet weak var image: UIImageView!
     
     @IBOutlet weak var label: UILabel!
+    var number: Int = 2
+    var minionCollection: [minion] = []
+    //型はminion
+    var kevin: minion = minion()
+    var stuart: minion = minion()
+    var bob: minion = minion()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -33,30 +39,29 @@ class ViewController: UIViewController {
         stuart.minionName = "stuart"
         kevin.minionPic = UIImage(named: "kevin3.png")
         kevin.minionName = "kevin"
-        minionCollection.append("bob")
-        minionCollection.append("stuart")
-        minionCollection.append("kevin")
+        //インスタンスをminionCollection:[minion] に追加していく
+        minionCollection.append(bob)
+        minionCollection.append(stuart)
+        minionCollection.append(kevin)
+//      minionCollectionの配列の中は、[bob,stuart,kevin]になっている
     }
-    
     @IBAction func back(_ sender: Any) {
         if number == 0 {
             number = 2
         } else {
             number -= 1
         }
-        image.image = UIImage(named: minionCollection[number])
-        label.text = minionCollection[number]
-        
+        image.image = minionCollection[number].minionPic
+        label.text = minionCollection[number].minionName
     }
-    
     @IBAction func next(_ sender: Any) {
         if number == 2 {
             number = 0
         } else {
             number += 1
         }
-        image.image = UIImage(named: minionCollection[number])
-        label.text = minionCollection[number]
+        image.image = minionCollection[number].minionPic
+        label.text = minionCollection[number].minionName
     }
 }
 
